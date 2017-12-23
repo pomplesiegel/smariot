69d8bead9c56110aafa4ad1d7461ddbf47b349d1
Bhargav Bhat <bhargavbhat@gmail.com>
2017-12-23 20:11:13 +0530

    change layout, implement GET

--- app.py
+++ app.py
@@ -7,23 +7,35 @@ from flask import Flask,render_template,request,jsonify,abort
 
 app = Flask(__name__)
 
-MAX_REQ_TO_KEEP = 5
+MAX_REQ_TO_KEEP = 5             # max. num requests to keep
+PAGE_RELOAD_INTERVAL = 240      # in seconds
+
 last_recv_data = deque(maxlen=MAX_REQ_TO_KEEP)
+
+def get_timestamp():
+    return strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
 
 @app.route("/")
 def default_route():
-    return render_template('index.html', result=last_recv_data, count=MAX_REQ_TO_KEEP, ts=strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime()))
+    return render_template('index.html', result=last_recv_data, count=MAX_REQ_TO_KEEP,
+                                ts=get_timestamp(), refresh=PAGE_RELOAD_INTERVAL)
 
-@app.route("/recv", methods=['POST']) 
+@app.route("/data", methods=['GET','POST']) 
 def index():
     resp = None
-    try:
-        global last_recv_data
-        last_recv_data.append(request.get_json(force=True))
-        resp = {'result':'success'}
+    if request.method == 'POST':
+        try:
+            global last_recv_data
+            last_recv_data.append((request.get_json(force=True), get_timestamp()))
+            resp = {'result':'success'}
+            return jsonify(resp)
+        except:
+            abort(400)
+    else:
+        resp = list()
+        for item in last_recv_data:
+            resp.append(item)
         return jsonify(resp)
-    except:
-        abort(400)
 
 if __name__ == '__main__':
     app.run(debug=True)
