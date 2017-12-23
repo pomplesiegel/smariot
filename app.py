66a83752efb081d10aaa3356725fb1acc4be8daf
Bhargav Bhat <bhargavbhat@gmail.com>
2017-12-23 15:29:55 +0530

    Parse and print TTN messages

--- app.py
+++ app.py
@@ -1,14 +1,38 @@
 #so that pipreqs adds gunicorn as a dep
 import gunicorn
-
-from flask import Flask
-from flask import render_template
+import base64
+from flask import Flask, render_template, redirect, url_for, request,jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
+def default_route():
+    return render_template('index.html')
+
+@app.route("/recv", methods=['POST']) 
 def index():
-    return render_template("index.html")
+    resp = None
+    try:
+        req_json = request.get_json(force=True)
+        #parse data
+        if parse_ttn_message(req_json):
+            resp = {'result':'success'}
+        else:
+            resp = {'result':'failure', 'error':'could not parse request'}
+    except:
+        resp = {'result':'failure', 'error':'request does not contain JSON'}
+    return jsonify(resp)
+
+def parse_ttn_message(msg):
+    try:
+        recv_data = {}
+        recv_data['hardware_serial'] = msg['hardware_serial']
+        recv_data['timestamp'] = msg['metadata']['time']
+        recv_data['payload_raw'] = base64.b64decode(msg['payload_raw'])
+        print(recv_data)
+        return True
+    except:
+        return False
 
 if __name__ == '__main__':
     app.run()
